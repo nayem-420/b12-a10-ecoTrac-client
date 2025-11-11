8f70d2b51ca2bfd50e7cf338664497cf4932b32c
nayem181 <nayemimranhasan@gmail.com>
2025-11-11 21:01:30 +0600

    finishing navbar and added footer

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import logo from '../assets/ecoTrac-logo.png';
+import logo from "../assets/ecoTrac-logo.png";
+import { NavLink } from "react-router";
 
 const Navbar = () => {
+  const activeRoutes = ({ isActive }) =>
+    isActive
+      ? "bg-gradient-to-r from-green-500 via-emerald-400 to-teal-400 text-white font-semibold rounded-md px-3 py-1 shadow-md transition-all duration-300"
+      : "text-gray-700 hover:text-green-600 hover:bg-gradient-to-r hover:from-green-100 hover:to-teal-100 rounded-md px-3 py-1 transition-all duration-300";
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
@@ -28,38 +33,50 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a>Item 1</a>
+              <NavLink className={activeRoutes} to={"/"}>
+                Home
+              </NavLink>
             </li>
             <li>
-              <a>Parent</a>
+              <NavLink className={activeRoutes} to={"/challenges"}>
+                Challenges
+              </NavLink>
             </li>
             <li>
-              <a>Item 3</a>
+              <NavLink className={activeRoutes} to={"/my-activities"}>
+                MyActivities
+              </NavLink>
             </li>
           </ul>
         </div>
         <div className="btn btn-ghost">
           <img src={logo} className="w-8 rounded-full" alt="" />
-          <a className="text-xl font-bold">
+          <NavLink to={'/'} className="text-xl font-bold">
             eco<span className="text-blue-500">Trac</span>
-          </a>
+          </NavLink>
         </div>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
+            <NavLink className={activeRoutes} to={"/"}>
+              Home
+            </NavLink>
           </li>
           <li>
-            <a>Parent</a>
+            <NavLink className={activeRoutes} to={"/challenges"}>
+              Challenges
+            </NavLink>
           </li>
           <li>
-            <a>Item 3</a>
+            <NavLink className={activeRoutes} to={"/my-activities"}>
+              MyActivities
+            </NavLink>
           </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn">Login</a>
       </div>
     </div>
   );
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router";
 import MainLayOuts from "../Layouts/MainLayOuts";
+import Challenges from "../Pages/Challenges";
+import MyActivities from "../Pages/MyActivities";
+import Home from "../Pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayOuts></MainLayOuts>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: "/challenges",
+        element: <Challenges></Challenges>,
+      },
+      {
+        path: "/my-activities",
+        element: <MyActivities></MyActivities>,
+      },
+    ],
   },
   {
     path: "/auth",
